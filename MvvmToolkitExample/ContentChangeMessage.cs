162b42d419a23821231b473f602aba3b7255d1c3
jacobchen <854312905@qq.com>
2024-03-28 09:59:23 +0800

    添加项目文件。

---
+++ MvvmToolkitExample/ContentChangeMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmToolkitExample
+{
+    public class ContentChangeMessage
+    {
+        public ContentChangeMessage(string msg)
+        {
+            Content = msg;
+        }
+        //消息内容
+        public string Content;
+    }
+}
---
+++ MvvmToolkitExample/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmToolkitExample
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            WeakReferenceMessenger.Default.Register<ContentChangeMessage>(this, (r, m) =>
+            {
+                Content = m.Content;
+            });
+        }
+
+        private int times = 0;
+
+        [ObservableProperty]
+        private string content = "通过MVVM方式，将字符串属性绑定到控件内容上";
+
+        //当 Content 属性被修改后（赋值后），会调用该方法。
+        partial void OnContentChanged(string? oldValue, string newValue)
+        {
+            times++;
+            content = $"{newValue} : { times }";
+        }
+
+        [RelayCommand]
+        private void ChangeContent()
+        {
+            Content = "Content 已通过命令方式修改";
+        }
+
+        [RelayCommand]
+        private void ChangeContentWithParam(string msg)
+        {
+            Content = msg;
+        }
+
+        [RelayCommand]
+        private void SendMessage(string msg)
+        {
+            WeakReferenceMessenger.Default.Send(new ContentChangeMessage(msg));
+        }
+
+    }
+
+
+}
